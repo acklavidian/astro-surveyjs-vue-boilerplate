73b6e0a0c43adda6d54db4dd4f8dac23ae4390c0
Eddie Stanley <acklavidian@gmail.com>
2024-06-02 13:41:23 -0500

    Fixing rendering issue with coveted SurveyJS components

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,6 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue({
-    appEntrypoint:'/src/pages/_app',
-    template: {
-      compilerOptions: {
-        isCustomElement: (tag) => ['SurveyCreatorComponent'].includes(tag),
-      }
-    }
+    appEntrypoint:'/src/pages/_app'
   })]
 });
